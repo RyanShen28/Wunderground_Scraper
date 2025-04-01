757a36e1ae59e9a9db8fcee14e85e231cee7d624
Ryan Shen <ryanshenpek@gmail.com>
2025-04-01 00:31:01 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+#YYYY - M - D
+#do not add 0 before one digit numbers
+START_DATE = date(2022, 1, 1)
+END_DATE = date(2022, 12, 31)
+#metric/imperial
+UNITS = "metric"
+
+
+# Automatically find first date where data is logged
+FIND_FIRST_DATE = False
